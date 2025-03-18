7d7e1492d38daa418fd43d7229487a035a7ef821
kldfk32 <94075390+kldfk32@users.noreply.github.com>
2025-03-18 17:55:29 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -2,7 +2,7 @@ import { ContentfulContentSource } from '@stackbit/cms-contentful';
 import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
-  stackbitVersion: '~0.6.0',
+  stackbitVersion: '~0.7.0',
   ssgName: 'nextjs',
   nodeVersion: '16',
   contentSources: [
