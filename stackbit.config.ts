6ba040e8638c8ca3aedee0e9b68eaa2ba97f449a
kldfk32 <94075390+kldfk32@users.noreply.github.com>
2025-03-18 18:13:30 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,20 +1,29 @@
-npm install --save-dev @stackbit/types @stackbit/sdk
-import { ContentfulContentSource } from '@stackbit/cms-contentful';
+import { GitContentSource } from '@stackbit/cms-git';
 import { defineStackbitConfig } from '@stackbit/types';
+import { allModels } from './.stackbit/models';
 
-export default defineStackbitConfig({
-  stackbitVersion: '~0.7.0',
-  ssgName: 'nextjs',
-  nodeVersion: '16',
-  contentSources: [
-    new ContentfulContentSource({
-      spaceId: process.env.CONTENTFUL_SPACE_ID,
-      environment: process.env.CONTENTFUL_ENVIRONMENT,
-      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
-      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
-    }),
-  ],
-  models: {
-    page: { type: 'page', urlPath: '/{slug}' },
-  },
-}
+const gitContentSource = new GitContentSource({
+    rootPath: __dirname,
+    contentDirs: ['content'],
+    models: Object.values(allModels),
+    assetsConfig: {
+        referenceType: 'static',
+        staticDir: 'public',
+        uploadDir: 'images',
+        publicPath: '/'
+    }
+});
+
+export const sbConfig = defineStackbitConfig({
+    stackbitVersion: '~0.7.0',
+    ssgName: 'nextjs',
+    nodeVersion: '18',
+    contentSources: [gitContentSource],
+    presetSource: {
+        type: 'files',
+        presetDirs: ['.stackbit/presets']
+    },
+    styleObjectModelName: 'ThemeStyle'
+});
+
+export default sbConfig;
