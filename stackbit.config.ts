e12bc438e876248d3b63be376d949ddf9d736c87
kldfk32 <94075390+kldfk32@users.noreply.github.com>
2025-03-21 10:48:59 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -2,6 +2,7 @@ import { defineStackbitConfig, SiteMapEntry } from "@stackbit/types";
 import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
+  stackbitVersion: '~0.7.0',
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
